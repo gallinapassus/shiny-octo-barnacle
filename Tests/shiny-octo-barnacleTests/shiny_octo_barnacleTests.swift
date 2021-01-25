0f115c754a3498aa7b2e90dced8549c9e2ed4829
gallinapassus <32310884+gallinapassus@users.noreply.github.com>
2021-01-25 16:45:31 +0200

    Fixed borken test.

--- Tests/shiny-octo-barnacleTests/shiny_octo_barnacleTests.swift
+++ Tests/shiny-octo-barnacleTests/shiny_octo_barnacleTests.swift
@@ -6,7 +6,7 @@ final class shiny_octo_barnacleTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(shiny_octo_barnacle().text, "Hello, World!")
+        XCTAssertEqual(shiny_octo_barnacle().text, "Hello World!")
     }
 
     static var allTests = [
